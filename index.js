6a6c65f56afdaa7f435926316a2bef5d114e51e0
Yead <yead19@gmail.com>
2025-01-19 00:43:46 +0600

    user side done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const express = require('express');
 const cors = require('cors');
@@ -32,6 +32,7 @@ async function run() {
         // Send a ping to confirm a successful connection
 
         const userCollection = client.db('ParcelDB').collection('users')
+        const parcelCollection = client.db('ParcelDB').collection('parcels')
 
 
 
@@ -48,6 +49,104 @@ async function run() {
                 return res.send({ message: 'User already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+        app.get('/users', async (req, res) => {
+            const email = req.query.email
+            const filter = { email: email }
+            const result = await userCollection.find(filter).toArray()
+            res.send(result)
+        })
+        app.get('/all-users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+        app.patch('/user/:id', async (req, res) => {
+            const id = req.params.id
+            const user = req.body
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    name: user.name,
+                    age: user.age,
+                    phone: user.phone,
+                    address: user.address,
+                    photo: user.photo,
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+        app.patch('/user/role/:id', async (req, res) => {
+            const id = req.params.id
+            const user = req.body
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: user.role,
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+
+
+        //parcel
+        app.post('/parcels', async (req, res) => {
+            const parcel = req.body
+            const result = await parcelCollection.insertOne(parcel)
+            res.send(result)
+
+        })
+        app.get('/parcel/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await parcelCollection.findOne(query);
+            res.send(result);
+        });
+        app.patch('/parcel/:id', async (req, res) => {
+            const id = req.params.id;
+            const parcel = req.body
+            const query = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    name: parcel.name,
+                    email: parcel.email,
+                    number: parcel.number,
+                    parcelType: parcel.parcelType,
+                    parcelWeight: parcel.parcelWeight,
+                    receiverName: parcel.receiverName,
+                    receiverPhoneNumber: parcel.receiverPhoneNumber,
+                    deliveryAddress: parcel.deliveryAddress,
+                    deliveryDate: parcel.deliveryDate,
+                    latitude: parcel.latitude,
+                    longitude: parcel.longitude,
+                    price: parcel.price
+                }
+            }
+            const result = await parcelCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        });
+
+
+        //cancel parcel
+        app.patch('/parcel/cancel/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    status: 'Cancelled'
+                }
+            }
+            const result = await parcelCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+        app.get('/parcels', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await parcelCollection.find(query).toArray()
             res.send(result)
         })
 
